558c5d5f5b552840c914c94650d25d492cae8aaa
happysuperevil601 <happysuperevil601@gmail.com>
2021-10-20 14:47:37 +0600

    done

--- src/Components/ServiceDetails/ServiceDetails.js
+++ src/Components/ServiceDetails/ServiceDetails.js
@@ -21,26 +21,45 @@ const ServiceDetails = () => {
 
     const { name, description, image, price } = serviceDetails[0] || {}
     return (
-        <div>
-            <div className="container w-25 mt-4">
-                <div className="col ">
+        <div className="container mt-4  ">
+            <div className="row justify-content-center align-items-center ">
+                <div className="w-50">
                     <div className="card h-100 shadow-lg">
-                        <img src={image} className="card-img-top" alt="..." />
+                        <img src={image} className="card-img-top " alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Service:{name}</h5>
-                            <p className="card-text">Description:{description}</p>
-                            <h5 className="card-text text-bold">Price:${price}</h5>
+
+
+                            <p className="text-nowrap"><strong >Service:{name}</strong></p>
+                            <p className="card-text"> <strong>Description:</strong> {description}</p>
+
+                            <p className="text-nowrap"><strong >Price:</strong> ${price}</p>
                         </div>
 
                     </div>
                 </div>
             </div>
-
-
+            {/* <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center text-md-left p-2">
+                        <img src={image} className="img-fluid" alt="..." />
+                    </div>
+                    <div className="justify-content-center align-items-center">
+                        <h5 className="card-title">Service:{name}</h5>
+                        <p className="card-text">Description:{description}</p>
+                        <h5 className="card-text text-bold">Price:${price}</h5>
+                    </div>
+                </div>
+            </div> */}
 
 
 
         </div>
+
+
+
+
+
+
 
 
 
